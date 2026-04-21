8c91ea5ff288ed4cc06b095cba313818796852fa
guptaapoorv2005-sudo <guptaapoorv2005@gmail.com>
2026-04-21 22:51:09 +0530

    feat: add ESLint configuration for TypeScript and update lint script

---
+++ backend/eslint.config.js
@@ -0,0 +1,25 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**", "prisma/migrations/**"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
